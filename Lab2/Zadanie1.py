2215719e931dcc168c415a84df30cb10e20353aa
Dominika Jabłońska <92153602+D0minikaJ@users.noreply.github.com>
2022-10-13 13:25:26 +0200

    Create Zadanie1.py

---
+++ Lab2/Zadanie1.py
@@ -0,0 +1,65 @@
+from enum import Enum
+
+
+class ColorModel(Enum):
+    rgb = 0
+    hsv = 1
+    hsi = 2
+    hsl = 3
+    gray = 4  # obraz 2d
+
+class BaseImage:
+    data: np.ndarray  # tensor przechowujacy piksele obrazu
+    color_model: ColorModel  # atrybut przechowujacy biezacy model barw obrazu
+
+    def __init__(self, path: str) -> None:
+        """
+        inicjalizator wczytujacy obraz do atrybutu data na podstawie sciezki
+        """
+        pass
+
+    def save_img(self, path: str) -> None:
+        """
+        metoda zapisujaca obraz znajdujacy sie w atrybucie data do pliku
+        """
+        pass
+
+    def show_img(self) -> None:
+        """
+        metoda wyswietlajaca obraz znajdujacy sie w atrybucie data
+        """
+        pass
+
+    def get_layer(self, layer_id: int) -> 'BaseImage':
+        """
+        metoda zwracajaca warstwe o wskazanym indeksie
+        """
+        pass
+
+    def to_hsv(self) -> 'BaseImage':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsv
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
+
+    def to_hsi(self) -> 'BaseImage':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsi
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
+
+    def to_hsl(self) -> 'BaseImage':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsl
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
+
+    def to_rgb(self) -> 'BaseImage':
+        """
+        metoda dokonujaca konwersji obrazu w atrybucie data do modelu rgb
+        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        """
+        pass
